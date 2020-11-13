e2ccade52bbbf3a719cfdcb87f532b5d16b13cf5
DEV <dev@info.local>
2020-11-13 10:32:51 -0300

    Exercicio aula 13-11-2020

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula_13_11_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            int meses = (idade * 12);
+            int dias = idade * 365;
+            int horas = dias * 24;
+            int minutos = horas * 60;
+
+            System.Console.WriteLine("Idade em meses: "+meses);
+            System.Console.WriteLine("Idade em dias: "+dias);
+            System.Console.WriteLine("Idade em horas: "+horas);
+            System.Console.WriteLine("Idade em minutos: "+minutos);
+
+
+        }
+    }
+}
